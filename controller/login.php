11a151917fbc468e5e989beaedd750e705b8fd81
byjulie0 <julievieiramartins0@gmail.com>
2025-12-01 17:05:34 -0400

    FIX: iniciando estilização da tela login

---
+++ controller/login.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login - Faz ai</title>
+    <link rel="stylesheet" href="../view/login.css">
+</head>
+<body>
+    <header>
+        <div class="container_login">
+            <div class="form_login">
+                <div class="img_login">
+                    <img src="" alt="">
+                    <h2 class="h2_login">Login</h2>
+                </div>
+                <form action="" method="POST" id="myForm" class="formulario_login">
+                    <input type="text" name="usuario" id="" placeholder="Nome do usuário ou E-mail" class="input_login">
+                    <input type="password" name="" id="" placeholder="Senha" class="input_login">
+                    <div class="g-recaptcha" data-sitekey="6LdyqOUrAAAAAGCnu7xdDfJ4QovvUsJMRuOgUvOa"></div>
+                </form>
+                <div class="button_login">
+                    <button class="verde_login" >Login</button>
+                    <button class="vermelho_login">Cancelar</button>
+                </div>
+                <div class="p_login">
+                    <p class="p_login"><a href="">Esqueci minha senha</a></p>
+                    <p class="p_login"><a href="">Cadastra-se</a></p>
+                </div>
+
+            </div>
+        </div>
+    </header>
+    
+</body>
+</html>
