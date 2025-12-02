7754caca3a3572f0d5d9f83b0e2b52271f87ce8b
byjulie0 <julievieiramartins0@gmail.com>
2025-12-02 15:33:42 -0400

    FIX: ajustes no footer

--- controller/cadastro.php
+++ controller/cadastro.php
@@ -1,5 +1,5 @@
 <?php
-include "menu.php"
+include "menu.php";
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -8,36 +8,41 @@ include "menu.php"
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro - Faz ai</title>
     <link rel="stylesheet" href="../view/public/css/cadastro.css">
-    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
 </head>
+
 <body>
-    <header>
-        <div class="container_cadastro">
+
+    <div class="wrapper_cadastro">
+
+        <div class="content_cadastro">
             <div class="form_cadastro">
                 <div class="img_cadastro">
                     <img src="../view/public/imagens/logo.png" alt="">
                     <h2 class="h2_cadastro">Cadastro</h2>
                 </div>
+
                 <form action="" method="POST" id="myForm" class="formulario_cadastro">
-                    <input type="text" name="usuario" id="" placeholder="Nome do usuário" class="input_cadastro">
-                    <input type="email" name="email" id="" placeholder="E-mail" class="input_cadastro">
-                    <input type="password" name="senha" id="" placeholder="Senha" class="input_cadastro">
-                    <input type="password" name="confirmaSenha" id="" placeholder="Confirmar senha" class="input_cadastro">
-                    <div class="g-recaptcha" data-sitekey=""></div>
+                    <input type="text" name="usuario" placeholder="Nome do usuário" class="input_cadastro">
+                    <input type="email" name="email" placeholder="E-mail" class="input_cadastro">
+                    <input type="password" name="senha" placeholder="Senha" class="input_cadastro">
+                    <input type="password" name="confirmaSenha" placeholder="Confirmar senha" class="input_cadastro">
+                    <div class="g-recaptcha" data-sitekey="6LdyqOUrAAAAAGCnu7xdDfJ4QovvUsJMRuOgUvOa"></div>
                 </form>
+
                 <div class="button_cadastro">
                     <button class="vermelho_cadastro">Cancelar</button>
-                    <button class="verde_cadastro" >Cadastrar</button>
-                </div>
-                <div class="">
-                    <p class="p_cadastro">Já tem conta?<a class="a_cadastro" href="cadastro.php">Login</a></p>
+                    <button class="verde_cadastro">Cadastrar</button>
                 </div>
 
+                <div>
+                    <p class="p_cadastro">Já tem conta? <a href="cadastro.php">Login</a></p>
+                </div>
             </div>
         </div>
-        
+
         <?php include "footer.php"; ?>
-    </header>
-    
+
+    </div>
+
 </body>
 </html>
