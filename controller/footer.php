7657279d4c76fade2cd55b58d1197d3acdb07375
isa-miho <isabella.miho@gmail.com>
2025-12-02 15:13:24 -0400

    feat: adicionado footer as paginas

---
+++ controller/footer.php
@@ -0,0 +1,5 @@
+<link rel="stylesheet" href="../view/public/css/footer.css">
+
+<footer class="footer">
+    <p class="copyright">© 2025 Faz Aí. Todos os direitos reservados.</p>
+</footer>
--- controller/login.php
+++ controller/login.php
@@ -1,5 +1,5 @@
 <?php
-include "menu.php"
+include "menu.php";
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -8,32 +8,47 @@ include "menu.php"
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login - Faz ai</title>
     <link rel="stylesheet" href="../view/public/css/login.css">
+    <link rel="stylesheet" href="../view/public/css/footer.css">
 </head>
-<body>
-    <header>
-        <div class="container_login">
-            <div class="form_login">
-                <div class="img_login">
-                    <img src="../view/public/imagens/logo.png" alt="">
-                    <h2 class="h2_login">Login</h2>
-                </div>
-                <form action="" method="POST" id="myForm" class="formulario_login">
-                    <input type="text" name="usuario" id="" placeholder="Nome do usuário ou E-mail" class="input_login">
-                    <input type="password" name="" id="" placeholder="Senha" class="input_login">
-                    <div class="g-recaptcha" data-sitekey="6LdyqOUrAAAAAGCnu7xdDfJ4QovvUsJMRuOgUvOa"></div>
-                </form>
-                <div class="button_login">
-                    <button class="vermelho_login">Cancelar</button>
-                    <button class="verde_login" >Login</button>
-                </div>
-                <div class="p_login">
-                    <p class="p_login"><a href="">Esqueci minha senha</a></p>
-                    <p class="p_login"><a href="cadastro.php">Cadastre-se</a></p>
-                </div>
 
+<body>
+
+    <div class="wrapper_login">
+
+        <div class="content_login">
+
+            <div class="container_login">
+                <div class="form_login">
+
+                    <div class="img_login">
+                        <img src="../view/public/imagens/logo.png" alt="">
+                        <h2 class="h2_login">Login</h2>
+                    </div>
+
+                    <form action="" method="POST" id="myForm" class="formulario_login">
+                        <input type="text" name="usuario" placeholder="Nome do usuário ou E-mail" class="input_login">
+                        <input type="password" name="senha" placeholder="Senha" class="input_login">
+                        <div class="g-recaptcha" data-sitekey="6LdyqOUrAAAAAGCnu7xdDfJ4QovvUsJMRuOgUvOa"></div>
+                    </form>
+
+                    <div class="button_login">
+                        <button class="vermelho_login">Cancelar</button>
+                        <button class="verde_login">Login</button>
+                    </div>
+
+                    <div class="p_login">
+                        <p><a href="">Esqueci minha senha</a></p>
+                        <p><a href="cadastro.php">Cadastre-se</a></p>
+                    </div>
+
+                </div>
             </div>
+
         </div>
-    </header>
-    
+
+        <?php include "footer.php"; ?>
+
+    </div>
+
 </body>
 </html>
